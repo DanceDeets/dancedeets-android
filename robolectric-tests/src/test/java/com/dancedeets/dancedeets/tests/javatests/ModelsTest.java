a2b4c3a7e75bf6c0b1024e829da8faac8b95b173
Mike Lambert <mlambert@gmail.com>
2014-10-22 20:17:05 -0400

    Switch back to an "app" module, but also add a "robolectric-tests" module too. This seems to work, without any magic classpath hackery or IML hackery. Yay for "It Just Works" (after three days of hacking on solutions)
    Seems Models actually depend on some android code (Log and Bundle), so technically they are still robotests. Need to clean up that distinction. Maybe could move them into another app that didn't have android dependencies, and move the directory into something unassociated with the android app?
    
    Also had to fix a few things as part of doing this:
    
    - Need to run with JRE7, both in Android Studio (by setting the test's JRE to /Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home ), or on the commandline (via JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home ./gradlew test)
    
    - Couldn't find the android libraries, since it was running without android, but as a barebones java test. So I needed to add a ~/.m2/settings.xml with:
    <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
        xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
                          http://maven.apache.org/xsd/settings-1.0.0.xsd">
        <localRepository>/Applications/Android Studio.app/sdk/extras/android/m2repository</localRepository>
    </settings>
    http://www.sluse.com/view/18045712 et al
    
    - Had to redo the include/exclude, but in this manner instead of the robolectric stanza, since we aren't using the robolectric module anymore.
    """
    Execution failed for task ':robolectric-tests:test'.
    > superClassName is empty!
    """
    http://stackoverflow.com/questions/14814837/gradle-test-fails-with-error
    
    - Needed to copy the resources/ directory over, for use by the tests (which call getResource). Looks like a bug where resources are not copied, discussed here, with a fix in #13:
    https://code.google.com/p/android/issues/detail?id=64887#c13

--- robolectric-tests/src/test/java/com/dancedeets/dancedeets/tests/javatests/ModelsTest.java
+++ robolectric-tests/src/test/java/com/dancedeets/dancedeets/tests/javatests/ModelsTest.java
@@ -1,13 +1,14 @@
 package com.dancedeets.dancedeets.tests.javatests;
 
-import android.test.InstrumentationTestCase;
-
 import com.dancedeets.dancedeets.models.CoverData;
 import com.dancedeets.dancedeets.models.FullEvent;
 import com.dancedeets.dancedeets.models.Venue;
+import com.dancedeets.dancedeets.tests.robotests.RobolectricGradleTestRunner;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
@@ -22,12 +23,15 @@ import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Locale;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+
 /**
  * Created by lambert on 2014/10/19.
  */
-public class ModelsTest extends InstrumentationTestCase {
-
-    private static final String LOG_TAG = "ModelsTest";
+@RunWith(RobolectricGradleTestRunner.class)
+public class ModelsTest {
 
     private String readTextFile(InputStream inputStream) throws IOException {
         Writer writer = new StringWriter();
@@ -44,79 +48,89 @@ public class ModelsTest extends InstrumentationTestCase {
 
         return writer.toString();
     }
-    private JSONObject getJsonObjectFromResource(int resource) {
+
+    private JSONObject getJsonObjectFromResource(String path) {
+        InputStream inputStream = getClass().getResourceAsStream(path);
         try {
-            InputStream inputStream = getInstrumentation().getContext().getResources().openRawResource(resource);
             String jsonEvent = readTextFile(inputStream);
             return new JSONObject(jsonEvent);
         } catch (JSONException e) {
-            fail("JSONException when loading resource: " + resource + ": " + e);
+            fail("JSONException when loading resource: " + e);
         } catch (IOException e) {
-            fail("IOException when loading resource: " + resource + ": " + e);
+            fail("IOException when loading resource: " + e);
         }
         return null;
     }
 
+    @Test
     public void testVenueEverything() throws JSONException {
-        Venue venue = Venue.parse(getJsonObjectFromResource(com.dancedeets.dancedeets.tests.R.raw.venue_everything));
+        Venue venue = Venue.parse(getJsonObjectFromResource("venue_everything"));
 
         assertEquals("Venue Name", venue.getName());
-        assertEquals(41.0, venue.getLatLong().getLatitude());
-        assertEquals(-74.0, venue.getLatLong().getLongitude());
+        assertEquals(41.0, venue.getLatLong().getLatitude(), 0.01);
+        assertEquals(-74.0, venue.getLatLong().getLongitude(), 0.01);
     }
 
+    @Test
     public void testVenueEverythingExceptZip() throws JSONException {
-        Venue venue = Venue.parse(getJsonObjectFromResource(com.dancedeets.dancedeets.tests.R.raw.venue_everything_except_zip));
+        Venue venue = Venue.parse(getJsonObjectFromResource("venue_everything_except_zip"));
 
         assertEquals("Venue Name", venue.getName());
-        assertEquals(41.0, venue.getLatLong().getLatitude());
-        assertEquals(-74.0, venue.getLatLong().getLongitude());
+        assertEquals(41.0, venue.getLatLong().getLatitude(), 0.01);
+        assertEquals(-74.0, venue.getLatLong().getLongitude(), 0.01);
     }
 
+    @Test
     public void testVenueNameIdGeocodeOnly() throws JSONException {
-        Venue venue = Venue.parse(getJsonObjectFromResource(com.dancedeets.dancedeets.tests.R.raw.venue_name_id_geocode_only));
+        Venue venue = Venue.parse(getJsonObjectFromResource("venue_name_id_geocode_only"));
 
         assertEquals("Venue Name", venue.getName());
-        assertEquals(41.0, venue.getLatLong().getLatitude());
-        assertEquals(-74.0, venue.getLatLong().getLongitude());
+        assertEquals(41.0, venue.getLatLong().getLatitude(), 0.01);
+        assertEquals(-74.0, venue.getLatLong().getLongitude(), 0.01);
     }
 
+    @Test
     public void testVenueNameOnly() throws JSONException {
-        Venue venue = Venue.parse(getJsonObjectFromResource(com.dancedeets.dancedeets.tests.R.raw.venue_name_only));
+        Venue venue = Venue.parse(getJsonObjectFromResource("venue_name_only"));
 
         assertEquals("Venue Name", venue.getName());
         assertNull(venue.getLatLong());
     }
 
+    @Test
     public void testCoverUnsorted() throws JSONException {
-        CoverData coverData = CoverData.parse(getJsonObjectFromResource(com.dancedeets.dancedeets.tests.R.raw.cover_unsorted));
+        CoverData coverData = CoverData.parse(getJsonObjectFromResource("cover_unsorted"));
 
         assertEquals("Cover ID", coverData.getId());
         assertEquals(2048, coverData.getLargestCover().getHeight());
         assertEquals(1365, coverData.getLargestCover().getWidth());
     }
 
+    @Test
     public void testEventStartAndEndTime() throws JSONException {
-        FullEvent event = FullEvent.parse(getJsonObjectFromResource(com.dancedeets.dancedeets.tests.R.raw.fullevent_start_and_end_time));
+        FullEvent event = FullEvent.parse(getJsonObjectFromResource("fullevent_start_and_end_time"));
         assertEquals("Oct 15, 2014 10:00 PM", event.getStartTimeString(Locale.US));
         assertEquals("Oct 16, 2014 4:00 AM", event.getEndTimeString(Locale.US));
     }
 
+    @Test
     public void testEventStartTimeOnly() throws JSONException {
-        FullEvent event = FullEvent.parse(getJsonObjectFromResource(com.dancedeets.dancedeets.tests.R.raw.fullevent_no_end_time));
+        FullEvent event = FullEvent.parse(getJsonObjectFromResource("fullevent_no_end_time"));
         assertEquals("Oct 15, 2014 10:00 PM", event.getStartTimeString(Locale.US));
         assertNull(event.getEndTimeString(Locale.US));
         assertEquals(0, event.getEndTimeLong());
     }
 
+    @Test
     public void testEventAllDay() throws JSONException {
-        FullEvent event = FullEvent.parse(getJsonObjectFromResource(com.dancedeets.dancedeets.tests.R.raw.fullevent_allday));
+        FullEvent event = FullEvent.parse(getJsonObjectFromResource("fullevent_allday"));
         assertEquals("Oct 15, 2014", event.getStartTimeString(Locale.US));
         assertNull(event.getEndTimeString(Locale.US));
     }
 
+    @Test
     public void testFullEventSerialization() throws JSONException, ClassNotFoundException, IOException {
-        FullEvent event = FullEvent.parse(getJsonObjectFromResource(com.dancedeets.dancedeets.tests.R.raw.fullevent_example_json));
+        FullEvent event = FullEvent.parse(getJsonObjectFromResource("fullevent_example_json"));
 
         ByteArrayOutputStream baoStream = new ByteArrayOutputStream();
         ObjectOutputStream ooStream = new ObjectOutputStream(baoStream);
