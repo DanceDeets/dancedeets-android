c103152b1311d14037bce7d2292831320d2a82ee
Mike Lambert <mlambert@gmail.com>
2014-10-24 20:46:28 -0400

    Get a basic robolectric test up and running....had to move activity_event_list.xml around, so that robolectric finds it in the "choose best resource file" logic. See this issue I filed on it: https://github.com/robolectric/robolectric/issues/1340

--- robolectric-tests/src/test/java/com/dancedeets/android/robotests/RobolectricTest.java
+++ robolectric-tests/src/test/java/com/dancedeets/android/robotests/RobolectricTest.java
@@ -1,21 +1,29 @@
 package com.dancedeets.android.robotests;
 
-import android.app.Activity;
-
 import com.dancedeets.android.EventListActivity;
+import com.dancedeets.android.EventListFragment;
+import com.dancedeets.dancedeets.R;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
+import org.robolectric.util.ActivityController;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 @RunWith(RobolectricGradleTestRunner.class)
 public class RobolectricTest {
 
     @Test
     public void testSomething() throws Exception {
-        Activity activity = Robolectric.buildActivity(EventListActivity.class).create().get();
-        assertTrue(activity != null);
+        ActivityController<EventListActivity> activityController = Robolectric.buildActivity(EventListActivity.class);
+        EventListActivity activity = activityController.create().get();
+        assertNotNull(activity);
+        EventListFragment fragment = (EventListFragment)activity.getFragmentManager().findFragmentById(R.id.event_list_fragment);
+        assertNotNull(fragment);
+
+        assertNull(activity.findViewById(R.id.event_info_fragment));
+
     }
 }
