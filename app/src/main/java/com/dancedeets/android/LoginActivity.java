5d0c928dd37ee234b0d1c8b9978ac463f7f81332
Mike Lambert <mlambert@gmail.com>
2014-11-26 16:28:03 -0500

    Beginnings of passing the auth data to the server. Need to pass along the city, which requires refactoring a bunch of other code.

--- app/src/main/java/com/dancedeets/android/LoginActivity.java
+++ app/src/main/java/com/dancedeets/android/LoginActivity.java
@@ -4,13 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.android.volley.Request.Method;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.dancedeets.dancedeets.R;
-import com.facebook.Request;
-import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
-import com.facebook.model.GraphUser;
 import com.facebook.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 /**
  * Created by lambert on 2014/11/11.
@@ -19,36 +25,45 @@ public class LoginActivity extends FacebookActivity {
 
     private static final String LOG_TAG = "LoginActivity";
 
-    public static class MeCallback implements Request.GraphUserCallback {
+    static DateFormat isoDateTimeFormatWithTZ = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
 
-        Session mSession;
-
-        MeCallback(Session session) {
-            mSession = session;
+    void updateServerSessionAndLocation(Session session, String location) {
+        JSONObject jsonPayload = new JSONObject();
+        try {
+            jsonPayload.put("access_token", session.getAccessToken());
+            jsonPayload.put("access_token_expires", isoDateTimeFormatWithTZ.format(session.getExpirationDate()));
+            jsonPayload.put("location", location);
+            jsonPayload.put("client", "android");
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Error constructing request: " + e);
+            return;
         }
-        @Override
-        public void onCompleted(GraphUser user, Response response) {
-            // If the response is successful
-            if (mSession == Session.getActiveSession()) {
-                if (user != null) {
-                    // TODO: send this ID and Session to the server.
-                    //session.getAccessToken();
-                    //session.getPermissions();
-                    //session.getAuthorizationBundle();
-                    Log.i(LOG_TAG, "ID: " + user.getId());
-                    Log.i(LOG_TAG, "Name: " + user.getName());
-                }
-            }
-        }
+        JsonObjectRequest request = new JsonObjectRequest(
+                Method.POST,
+                "http://www.dancedeets.com/auth",
+                jsonPayload,
+                new com.android.volley.Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.i(LOG_TAG, "Successfully called /auth: " + response);
+                    }
+                }, new com.android.volley.Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e(LOG_TAG, "Error calling /auth: " + error);
+                    }
+                });
+        VolleySingleton.getInstance().getRequestQueue().add(request);
     }
+
     protected void onSessionStateChange(Session session, SessionState state, Exception exception) {
         // Don't call the super, since we don't want it sending us back to the LoginActivity when logged out
         // super.onSessionStateChange(session, state, exception);
         if (state.isOpened()) {
             Log.i(LOG_TAG, "Activity " + this + " is logged in, with state: " + state);
 
-            Request request = Request.newMeRequest(session, new MeCallback(session));
-            Request.executeBatchAsync(request);
+            //TODO(lambert): grab the current city and pass to this call
+            updateServerSessionAndLocation(session, "");
 
             Intent intent = new Intent(this, EventListActivity.class);
             intent.setAction(Intent.ACTION_DEFAULT);
