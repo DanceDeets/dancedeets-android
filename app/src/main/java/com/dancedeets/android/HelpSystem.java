f56958307f622d439f401fc4f8fa87ac06cc43ef
Mike Lambert <mlambert@gmail.com>
2015-01-22 19:16:31 -0500

    Add url language param for help and add-event pages.

--- app/src/main/java/com/dancedeets/android/HelpSystem.java
+++ app/src/main/java/com/dancedeets/android/HelpSystem.java
@@ -4,13 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 
+import java.util.Locale;
+
 /**
  * Created by lambert on 2015/01/21.
  */
 public class HelpSystem {
     public static void openHelp(Activity activity) {
         AnalyticsUtil.track("Open Help");
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.dancedeets.com/help"));
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.dancedeets.com/help?hl=" + Locale.getDefault().getLanguage()));
         if (intent.resolveActivity(activity.getPackageManager()) != null) {
             activity.startActivity(intent);
         }
@@ -18,7 +20,7 @@ public class HelpSystem {
 
     public static void openAddEvent(EventListActivity activity) {
         AnalyticsUtil.track("Add Event");
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.dancedeets.com/events_add"));
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.dancedeets.com/events_add?hl=" + Locale.getDefault().getLanguage()));
         if (intent.resolveActivity(activity.getPackageManager()) != null) {
             activity.startActivity(intent);
         }
