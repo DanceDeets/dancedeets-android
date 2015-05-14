7111389e084fc98c543e244a73178246bcaeaeae
Mike Lambert <mlambert@gmail.com>
2015-05-14 02:00:20 -0400

    Zip-Align our APK for release.

---
+++ zipalign.sh
@@ -0,0 +1,2 @@
+rm ~/Projects/dancedeets-newdroid/app/build/outputs/apk/app-release-aligned.apk
+~/Library/Android/sdk/build-tools/*/zipalign 4 ~/Projects/dancedeets-newdroid/app/build/outputs/apk/app-release-unaligned.apk ~/Projects/dancedeets-newdroid/app/build/outputs/apk/app-release-aligned.apk
