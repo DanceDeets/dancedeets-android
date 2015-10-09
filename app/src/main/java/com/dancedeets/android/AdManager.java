eb79969274a9cdeccccf08f639e66f2303c3d617
Mike Lambert <mlambert@gmail.com>
2015-10-09 00:37:35 -0700

    Factor out our MD5 function and use it to pass in a hashed UID to DfP for frequency capping purposes.

--- app/src/main/java/com/dancedeets/android/AdManager.java
+++ app/src/main/java/com/dancedeets/android/AdManager.java
@@ -2,6 +2,7 @@ package com.dancedeets.android;
 
 import android.location.Location;
 
+import com.facebook.AccessToken;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
 
@@ -11,11 +12,15 @@ import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
 public class AdManager {
 
     public static PublisherAdRequest getAdRequest(Location location) {
-        PublisherAdRequest adRequest = new PublisherAdRequest.Builder()
+        PublisherAdRequest.Builder adRequestBuilder = new PublisherAdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .addTestDevice("F92F46CF903B1E0BD86D386FC9813E7F")
-                .setLocation(location)
-                .build();
-        return adRequest;
+                .setLocation(location);
+        AccessToken token = AccessToken.getCurrentAccessToken();
+        if (token != null) {
+            String userID = token.getUserId();
+            adRequestBuilder.setPublisherProvidedId(Hashing.md5(userID));
+        }
+        return adRequestBuilder.build();
     }
 }
