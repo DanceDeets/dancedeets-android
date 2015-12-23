50c53f04b2d48c62656be3878cf1f7fa982f777e
Mike Lambert <mlambert@gmail.com>
2015-12-23 04:35:08 -0500

    Update zipalign.sh, though honestly looks like this is no longer necessary since Android Studio is producing proper zip-aligned APKs now.

--- zipalign.sh
+++ zipalign.sh
@@ -1,3 +1,4 @@
-ZIPALIGN=$(ls ~/Library/Android/sdk/build-tools/*/zipalign | grep -v '21.')
-rm ~/Projects/dancedeets-newdroid/app/build/outputs/apk/app-release-aligned.apk
-$ZIPALIGN 4 ~/Projects/dancedeets-newdroid/app/build/outputs/apk/app-release-unaligned.apk ~/Projects/dancedeets-newdroid/app/build/outputs/apk/app-release-aligned.apk
+ZIPALIGN=$(ls ~/Library/Android/sdk/build-tools/*/zipalign | grep -v '23.')
+BASEDIR=$(dirname $0)
+rm $BASEDIR/app/app-release-aligned.apk
+$ZIPALIGN 4 $BASEDIR/app/app-release.apk $BASEDIR/app/app-release-aligned.apk
