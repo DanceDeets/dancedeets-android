e41e5952e84aad9607f7067c27f5ccdbd53a473e
Mike Lambert <mlambert@gmail.com>
2015-12-23 04:35:39 -0500

    Delete zipalign.sh as it should no longer be necessary.

--- zipalign.sh
+++
@@ -1,4 +0,0 @@
-ZIPALIGN=$(ls ~/Library/Android/sdk/build-tools/*/zipalign | grep -v '23.')
-BASEDIR=$(dirname $0)
-rm $BASEDIR/app/app-release-aligned.apk
-$ZIPALIGN 4 $BASEDIR/app/app-release.apk $BASEDIR/app/app-release-aligned.apk
